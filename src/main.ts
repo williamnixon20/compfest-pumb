ebedae132ba6fdfa879e4918551e8e493ca9dff3
williamnixon20 <75229742+williamnixon20@users.noreply.github.com>
2022-08-07 09:54:52 +0700

    [ADD] Swagger Docs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  const port = process.env.PORT || 80;
-  console.log('Controller started at port ', port);
+  const port = process.env.PORT || 3000;
+  console.log('Controller started at port', port);
+  const config = new DocumentBuilder()
+    .setTitle('PUMB API Description')
+    .setDescription('')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
   await app.listen(port);
 }
 bootstrap();
