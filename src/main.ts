fabde4f7cb21b31fa40ca9d93059fe787c39444c
williamnixon20 <75229742+williamnixon20@users.noreply.github.com>
2022-08-09 11:52:28 +0700

    [CHG] Prisma setup

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const port = process.env.PORT || 3000;
   console.log('Controller started at port', port);
+
   const config = new DocumentBuilder()
     .setTitle('PUMB API Description')
     .setDescription('')
@@ -13,6 +15,7 @@ async function bootstrap() {
     .build();
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('', app, document);
   await app.listen(port);
