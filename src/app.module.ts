0cbb37dbe840b082ae23973d205e2bb0987fb23d
williamnixon20 <75229742+williamnixon20@users.noreply.github.com>
2022-08-11 15:19:42 +0700

    [DEL] Courses Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,9 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/jwt-auth';
-import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UsersModule, CoursesModule],
+  imports: [PrismaModule, AuthModule, UsersModule],
   controllers: [],
   providers: [
     {
